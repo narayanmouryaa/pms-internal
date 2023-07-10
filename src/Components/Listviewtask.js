b5d6398a0feff7c21076cdd2d5ce08887ad3c065
narayanmouryaa <mouryanarayan1998@gmail.com>
2023-07-11 01:04:36 +0530

    add some new functionality

--- src/Components/Listviewtask.js
+++ src/Components/Listviewtask.js
@@ -35,79 +35,79 @@ const SubtaskList = () => {
 
     return (
         <>
-        <Box style={{ display: 'flex', alignItems: 'center'}}>
-            <div>
-                <List>
-                    {subtasks.map((subtask, index) => (
-                        <ListItem key={index}>
+            <Box style={{ display: 'flex', alignItems: 'center' }}>
+                <div>
+                    <List>
+                        {subtasks.map((subtask, index) => (
+                            <ListItem key={index}>
 
 
-                            <FormControlLabel
-                                control={<Checkbox />}
+                                <FormControlLabel
+                                    control={<Checkbox />}
                                 // onChange={() => handleDeleteSubtask(index)}
 
-                            />
-                            <Avatar sx={{ height: 30, width: 30 }}>A</Avatar>
+                                />
+                                <Avatar sx={{ height: 30, width: 30 }}>A</Avatar>
 
 
-                            <ListItemText primary={subtask} style={{ marginLeft: '10px' }} />
+                                <ListItemText primary={subtask} style={{ marginLeft: '10px' }} />
 
 
-                        </ListItem>
-                    ))}
-                </List>
-                <TextField
-                    label="Add Task"
-                    style={{ width: '1100px',borderRadius:'0px' }}
+                            </ListItem>
+                        ))}
+                    </List>
+                    <TextField
+                        label="Add Task"
+                        style={{ width: '1100px', borderRadius: '0px' }}
 
-                //     inputProps={{
-                //     endAdornment :(
-                //         <>
-                //         <Nav.Link href="#" onClick={() => setOpenModal(true)}>
-                //      <Button style={{ color: 'black', display: 'inline' }} >
-                //        value={newSubtask}
-                //      </Button>
-                //    </Nav.Link> 
-                // </>
+                        //     inputProps={{
+                        //     endAdornment :(
+                        //         <>
+                        //         <Nav.Link href="#" onClick={() => setOpenModal(true)}>
+                        //      <Button style={{ color: 'black', display: 'inline' }} >
+                        //        value={newSubtask}
+                        //      </Button>
+                        //    </Nav.Link> 
+                        // </>
 
-                //     )
-                // }}
-                    
-                    
+                        //     )
+                        // }}
 
-                 value={newSubtask}
 
-                    onChange={(e) => setNewSubtask(e.target.value)}
-                    InputProps={{
-                        endAdornment: (
-                            <>
 
-                                {/* <Tooltip title="Start Date" placement="top">
+                        value={newSubtask}
+
+                        onChange={(e) => setNewSubtask(e.target.value)}
+                        InputProps={{
+                            endAdornment: (
+                                <>
+
+                                    {/* <Tooltip title="Start Date" placement="top">
                                     <CalendarIcon  />
                                 </Tooltip> */}
 
-                                <Tooltip title="Due Date" placement="top">
-                                    <CalendarIcon />
-                                </Tooltip>
+                                    <Tooltip title="Due Date" placement="top">
+                                        <CalendarIcon />
+                                    </Tooltip>
 
-                                <PersonIcon />
-                                <FlagIconDropdown/>
-                                <Button variant="contained" onClick={handleAddSubtask} sx={{ padding: '10px' ,borderRadius:'0px'}}>
-                    Save
-                </Button>
+                                    <PersonIcon />
+                                    <FlagIconDropdown />
+                                    <Button variant="contained" onClick={handleAddSubtask} sx={{ padding: '10px', borderRadius: '0px' }}>
+                                        Save
+                                    </Button>
 
-                            </>
-                        )
-                    }}
+                                </>
+                            )
+                        }}
 
 
-                />
+                    />
 
-                
-            </div>
 
-        </Box>
-        {/* <TaskModal open={openModal} setOpen={setOpenModal} /> */}
+                </div>
+
+            </Box>
+            {/* <TaskModal open={openModal} setOpen={setOpenModal} /> */}
 
 
         </>
